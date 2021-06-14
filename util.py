20181c4691a38b78a55cb1905655fed4ab128735
Antoine Vendeville <antoinevendeville@eduroam-int-dhcp-97-97-126.ucl.ac.uk>
2021-06-14 13:28:15 +0100

    up

--- util.py
+++ util.py
@@ -2,10 +2,10 @@
 """ Custom functions used in main code """
 
 import numpy as np
-from scipy.linalg import expm
+import networkx as nx
+from scipy.linalg import expm, null_space
 
-
-# simulations
+# voter model with zealots in both camps
 def voter_simu(n, n1, s1, s0, max_time, spacing, seed=None):
         
     # init
@@ -73,7 +73,6 @@ def mixing_time(n, n1, s0, s1, Q, pi, eps, spacing):
         Pt = expm(t*Q)
         totalvar = 0.5 * np.abs(Pt[n1-s1,:]-pi).sum()
         t += spacing
-<<<<<<< HEAD
     return t
 
 
@@ -127,6 +126,213 @@ def polarisation(neighbors, clique, N1, S1, S0, max_time, spacing, seed=None):
     
     # end
     return N1t
-=======
-    return t
->>>>>>> 2eab0c81d67a985a9c60d55a7b54b2a98cc7ecfa
+
+
+
+# stubborn in both camps with custom graph
+def custom_graph_simu(leaders, n1, s1, s0, max_time, spacing, seed=None):
+    """leaders must be a dict with numpy arrays as values"""
+        
+    # init
+    n = len(leaders)
+    np.random.seed(seed)
+    opinion = np.random.permutation([1]*n1 + [0]*(n-n1))
+    N1 = [int(n1)]
+    n1_tmp = int(n1)
+    
+    # stubborn
+    stub0 = np.random.choice(np.where(opinion==0)[0], size=s0, replace=False)
+    stub1 = np.random.choice(np.where(opinion==1)[0], size=s1, replace=False)
+    
+    # iter
+    t, t_sum = 0, 0
+    while t<max_time:
+        u = np.random.choice(range(n))
+        lead = leaders[u]
+        if u not in stub0 and u not in stub1 and lead.size>0: # skip if stubborn or no leaders
+            old_opi = opinion[u]
+            opinion[u] = np.random.choice(opinion[lead])
+            n1_tmp += opinion[u] - old_opi
+        waiting = np.random.exponential(1/n) # random exp of param n
+        t, t_sum = t+waiting, t_sum+waiting
+        if t_sum >= spacing:
+            N1.append(int(n1_tmp))
+            t_sum = 0
+            
+    # complete if needed
+    length = int(np.floor(max_time/spacing)+1)
+    if len(N1) < length:
+        N1 += [n1_tmp]*(length-len(N1))
+    
+    # end
+    return np.array(N1)
+
+
+
+# custom function to create a follwoers dict for an ER graph of cosen parameter, directed or not
+# /!\ RETURNS LEADERS DICT (and not followers)
+def create_ER_graph(n,p, directed=False):
+    leaders = {i:np.array([], dtype=int) for i in range(n)}
+    
+    if directed:
+        for i in range(n):
+            for j in range(i):
+                if np.random.random() < p:
+                    leaders[i] = np.append(leaders[i],j)
+                if np.random.random() < p:
+                    leaders[j] = np.append(leaders[j],i)
+                
+    else:
+        for i in range(n):
+            for j in range(i):
+                if np.random.random() < p:
+                    leaders[i] = np.append(leaders[i],j)
+                    leaders[j] = np.append(leaders[j],i)
+        
+    return leaders
+
+# discrete chain transitions
+def Pmatrix(n,z0,z1):
+    P = np.zeros((n-z0-z1+1, n-z0-z1+1))
+    for k in range(z1,n-z0+1):
+        P[k-z1,k-z1-1] = (k-z1)/n * (n-k)/(n-1)
+        if k<n-z0:
+            P[k-z1,k-z1+1] = (n-k-z0)/n * k/(n-1)
+            P[k-z1,k-z1] = 1 - P[k-z1,k-z1-1] - P[k-z1,k-z1+1]
+        else:
+            P[k-z1,k-z1] = 1 - P[k-z1,k-z1-1]
+    return P
+
+
+# discrete chain stationary
+def stationary_discrete(P):
+    pi = null_space(P.T-np.identity(P.shape[0]))
+    pi = pi/pi.sum() # normalise
+    pi = np.squeeze(pi) # flatten
+    return pi
+
+
+# distribution of the chain at step t according to spectral representation, can choose discrete or continuous chain
+def distrib_spectral(n,z0,z1,n1,t,mode="continuous"):
+    
+    # get S matrix and its spectrum
+    P = Pmatrix(n,z0,z1)
+    pi_sqrt = np.sqrt(stationary_discrete(P))
+    S = np.diag(pi_sqrt).dot(P).dot(np.diag(1/pi_sqrt))
+    L, U = np.linalg.eig(S) # eigval, eigvec
+    
+    # sort eigvals and eigvecs
+    idx = L.argsort()[::-1]
+    L = L[idx]
+    U = U[:,idx]
+    
+    # compute stationary 
+    res = list()
+    if mode == "continuous":
+        for k in range(z1,n-z0+1):
+            val = pi_sqrt[k-z1]/pi_sqrt[n1-z1] * (np.exp(L-1)*t * U[:,n1-z1] * U[:,k-z1]).sum()
+            res.append(val)
+    elif mode == "discrete":
+        for k in range(z1,n-z0+1):
+            val = pi_sqrt[k-z1]/pi_sqrt[n1-z1] * (L**t * U[:,n1-z1] * U[:,k-z1]).sum()
+            res.append(val)
+    else:
+        print("mode should be continuous or discrete")
+        
+    # end
+    return np.array(res)
+
+
+
+######################## OPTIM PROBLEM ###########################
+# ideal z1 (no backfire)
+def z1_ideal(z0,lambd):
+    return int(z0*lambd/(1-lambd))
+
+# ideal z1 (with backfire)
+def z1_ideal_backfire(z0,lambd,alpha):
+    D = 1-lambd-alpha*lambd*z0
+    if D>0:
+        return int(lambd*z0/D)
+    else:
+        return None
+
+# cv time of coalescing random walks on complete graph with n nodes and z total zealots
+def coalesce_cvtime(n,z):
+    res = 0
+    for k in range(1,n-z+1):
+        res += 1/(k*(k+z-1))
+    res *= n-1
+    return res
+
+# create a leaders dict for a specified graph model
+def create_connected_user_graph(n,model,param): #param should be w for ER, m for BA, (k_ws,p) for WS, lambda for SF
+    connected = False # restart if we don't get a connected graph
+    while not connected:
+        if model=="ER":
+            G = nx.erdos_renyi_graph(n,param)
+        elif model == "BA":
+            G = nx.barabasi_albert_graph(n,param)
+        elif model == "WS":
+            G = nx.watts_strogatz_graph(n,4,param) # initially 4 neighborus for each node
+        elif model == "SF":
+            even = False
+            while not even: # retstart as long as sum of degrees is not even
+                deg_seq = np.random.zipf(param,n)
+                even = deg_seq.sum()%2==0
+            G = nx.configuration_model(deg_seq)
+            G = nx.Graph(G) # remove multi-edges
+            G.remove_edges_from(nx.selfloop_edges(G)) # remove self-loops
+        else: 
+            print("incompatible graph model: {}".format(model))
+            return None
+        connected = nx.is_connected(G)
+    return {i: np.array(list(G.neighbors(i))) for i in range(n)} # return a leader dict
+
+# bound on cv time with n nodes and z total zealots
+def bound_cvtime(n,z):
+    return np.exp(1)*(1+np.log(n-z))*(n-1)/(z-1)
+
+# greedy algo without backfire effect
+def greedy_optim(n,z0,lambd,zmax,T): #cv=cv function
+    # init
+    z1 = min(zmax, z1_ideal(z0,lambd))
+    t = coalesce_cvtime(n,z0+z1)
+    # iter
+    n_iter = 0
+    while t>T or z1>zmax:
+        if t>T and z1>=zmax:
+            return -1, -1, n_iter
+        elif t>T and z1<zmax:
+            z1 += 1
+        elif t<T and z1>zmax:
+            z1 -= 1
+        else: # for debug
+            return "bug"
+        t = coalesce_cvtime(n,z0+z1)
+        n_iter += 1 
+    # end
+    return z1, t, n_iter
+
+# greedy algo with backfire effect and converting method
+def greedy_optim_backfire(n,z0,lambd,alpha,zmax,T):
+    n_iter = 0
+    z1_ideal_tmp = z1_ideal_backfire(z0,lambd,alpha)
+    if z1_ideal_tmp==None:
+        return -1, -1, n_iter
+    else:
+        z1 = min(zmax, z1_ideal_tmp)
+        t = coalesce_cvtime(n,z0+z1)
+        while t>T or z1>zmax:
+            if t>T and z1>=zmax:
+                return -1, -1, n_iter
+            elif t>T and z1<zmax:
+                z1 += 1
+            elif t<T and z1>zmax:
+                z1 -= 1
+            else: # for debug
+                return "bug"
+            t = coalesce_cvtime(n,z0+z1)
+            n_iter += 1 
+        # end
+        return z1, t, n_iter
